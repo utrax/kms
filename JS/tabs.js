d749df2262f26861a5f8ad4f2f4e5843597abc2d
Sisay Sorsa <55367509+utrax@users.noreply.github.com>
2022-02-05 22:45:55 +0300

    Create tabs.js

---
+++ JS/tabs.js
@@ -0,0 +1,14 @@
+// tabs javascript behaviours
+function openCity(evt, tpanels) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(tpanels).style.display = "block";
+  evt.currentTarget.className += " active";
+}
